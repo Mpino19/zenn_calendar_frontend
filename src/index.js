9100dc5ddeaa99ac382ec2b50842790df8c1ff20
Mpino19 <mykmm19@gmail.com>
2024-08-25 15:28:45 +0900

    ルーティングを設定

--- src/index.js
+++ src/index.js
@@ -4,15 +4,23 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ChakraProvider } from "@chakra-ui/react";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SignUp from "./routes/SignUp";
+import Calendar from "./routes/Calendar";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<App />} />
+          <Route path="calendar" element={<Calendar />} />
+          <Route path="signUp" element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
     </ChakraProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
